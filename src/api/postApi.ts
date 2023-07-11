8beb20dcdca924f1d7e99802080d24cd4fdf2032
nguyentoan06 <toan@myspa.vn>
2023-07-11 15:58:42 +0700

    [UPDATE] Dev_Toan

--- src/api/postApi.ts
+++ src/api/postApi.ts
@@ -5,10 +5,10 @@ import { ParamsPost } from "params-query/param.interface";
 
 export const postApi = {
   posts: (pr: ParamsPost) => {
-    return axiosClient.get(API_ROUTE.POSTS, { params: pr }).then<ResponseType<IPost[]>>(res => res.data)
+    return axiosClient.get('post', { params: pr }).then<ResponseType<IPost[]>>(res => res.data)
   },
   post: (id: number | string) => {
-    return axiosClient.get(API_ROUTE.POST(id)).then<Response<IPost>>(res => res.data)
+    return axiosClient.get(`/post/${id}`, { params: { 'append': 'media_url' } }).then<Response<IPost>>(res => res.data)
   },
   createPost: (body: ReqPost) => {
     return axiosClient.post(API_ROUTE.POSTS, body).then<Response<IPost>>(res => res.data)
