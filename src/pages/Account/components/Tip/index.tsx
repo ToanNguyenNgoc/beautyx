b7d1a4c154783d82b021fbfd3d4ab23378534f1b
Đỗ Cao Long <long@myspa.vn>
2024-05-20 09:23:33 +0700

    [UPDATE] fix UI tips

--- src/pages/Account/components/Tip/index.tsx
+++ src/pages/Account/components/Tip/index.tsx
@@ -37,7 +37,6 @@ export default function Tip() {
                 settings: {
                     fade: false,
                     speed: 600,
-                    adaptiveHeight: true,
                 },
             },
         ],
